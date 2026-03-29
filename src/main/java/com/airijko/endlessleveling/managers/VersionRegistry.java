0bf287db7e5493aa46ac62600e0f578a8aed10ac
jojoj <jojojimenajr@gmail.com>
2026-03-28 23:57:36 -0500

    support buff 7.0.2

--- src/main/java/com/airijko/endlessleveling/managers/VersionRegistry.java
+++ src/main/java/com/airijko/endlessleveling/managers/VersionRegistry.java
@@ -21,8 +21,8 @@ public final class VersionRegistry {
     public static final int PLAYERDATA_SCHEMA_VERSION = 19;
 
     public static final int BUILTIN_AUGMENTS_VERSION = 65;
-    public static final int BUILTIN_CLASSES_VERSION = 34;
-    public static final int BUILTIN_RACES_VERSION = 34;
+    public static final int BUILTIN_CLASSES_VERSION = 35;
+    public static final int BUILTIN_RACES_VERSION = 35;
     public static final int BUILTIN_LANG_VERSION = 5;
     public static final int BUILTIN_WORLD_SETTINGS_VERSION = 3;
 
